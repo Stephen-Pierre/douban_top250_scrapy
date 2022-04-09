61f6a78ec820a6feffdd390a8a397375b3f353be
pierre <stephen_pierre@163.com>
2022-04-09 23:47:53 +0800

    提交主要代码

---
+++ douban_movie/douban_movie/spiders/doubanmovie.py
@@ -0,0 +1,25 @@
+import scrapy
+from douban_movie.items import DoubanMovieItem
+
+class DoubanmovieSpider(scrapy.Spider):
+    name = 'doubanmovie'
+    allowed_domains = ['douban.com']
+    start_urls = ['https://movie.douban.com/top250']
+
+    def parse(self, response):
+        el_list = response.xpath('//*[@id="content"]//div[@class="info"]')
+        print(len(el_list))
+
+        for res in el_list:
+            item = DoubanMovieItem()
+            item['name'] = res.xpath('./div[1]/a/span[1]/text()').extract_first()
+            yield item
+
+        next_page_url = response.xpath('//span[@class="next"]/a/@href').extract_first()
+        if next_page_url:
+            url = response.urljoin(next_page_url)
+            yield scrapy.Request(
+                url=url
+            )
+
+
